0a7f01f83139f6a477f2905b63bbb057b599724b
Arkkul <104141207+Arkkul@users.noreply.github.com>
2023-04-21 09:47:15 +0300

    1.2
     синхронизация

--- Tele2Victo/Assets/Scripts/Synchronization.cs
+++ Tele2Victo/Assets/Scripts/Synchronization.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class Synchronization : MonoBehaviour
 {
 
-    private string serverURL = "http://f0793905.xsph.ru";
+    private string serverURL = "http://localhost/tehnostrelka";
     [SerializeField] Text phone;
 
     public void Login()
@@ -40,5 +40,77 @@ public class Synchronization : MonoBehaviour
     {
         PlayerPrefs.SetString("phone", $"+7{login}");
     }
-   
+
+    public void setData()
+    {
+        StartCoroutine(UploadUserData());
+    }
+
+    public IEnumerator UploadUserData() //загрузка информации
+    {
+
+        CheckData();
+        WWWForm form = new WWWForm(); //создание формы для отправки
+     
+        form.AddField("Login", PlayerPrefs.GetString("phone"));
+        form.AddField("Points", PlayerPrefs.GetString("points"));
+
+        WWW www = new WWW($"{serverURL}/uploadData.php", form); //отправка формы на сервер
+        yield return www;
+
+        if (www.error != null) //проверка на ошибки
+        {
+            Debug.Log(www.error);
+            yield break;
+        }
+        else
+        {
+            Debug.Log("Сервер ответил: " + www.text); //ответ сервера в www.text
+
+        }
+    }
+
+    public IEnumerator GetUserData() //загрузка информации
+    {
+
+        CheckData();
+        WWWForm form = new WWWForm(); //создание формы для отправки
+
+        form.AddField("Login", PlayerPrefs.GetString("phone"));
+        //form.AddField("Points", PlayerPrefs.GetString("points"));
+
+        WWW www = new WWW($"{serverURL}/getData.php", form); //отправка формы на сервер
+        yield return www;
+
+        if (www.error != null) //проверка на ошибки
+        {
+            Debug.Log(www.error);
+            yield break;
+        }
+        else
+        {
+            Debug.Log("Сервер ответил: " + www.text); //ответ сервера в www.text
+            PlayerPrefs.SetInt("points", int.Parse(www.text));
+        }
+    }
+    public void getData()
+    {
+        StartCoroutine(GetUserData());
+    }
+
+    void CheckData()
+    {
+        if(PlayerPrefs.HasKey("points")) //проверка на существующие данные
+
+        {
+
+
+        }
+        else
+        {
+            PlayerPrefs.SetInt("points", 0);
+        }
+       
+    }
+
 }
